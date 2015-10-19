d1c1cec6b08b315ff8e2abde5baa983dccfec96f
Dimas Julisa Taniawan <dimas@41studio.com>
2015-10-19 11:16:25 +0700

    disable unused generators

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,12 @@ module Bottle
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # disable unused generators
+    config.generators do |generate|
+      generate.helper false
+      generate.assets false
+      generate.view_specs false
+    end
   end
 end
