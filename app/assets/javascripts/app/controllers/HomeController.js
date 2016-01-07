35b4de8d38d0fa7388f9e016bf8bc67ac75e16ae
radiet <radiet@41studio.com>
2016-01-07 11:10:51 +0700

    move function from mainController to HomeController to avoid rootScope

--- app/assets/javascripts/app/controllers/HomeController.js
+++ app/assets/javascripts/app/controllers/HomeController.js
@@ -1,5 +1,5 @@
-app.controller('HomeController', ['$scope', 'Restangular',
-  function($scope, Restangular){
+app.controller('HomeController', ['$scope', '$rootScope', 'Restangular', 'PubNub',
+  function($scope, $rootScope, Restangular, PubNub){
     $scope.isDj = function(){
       if(window.location.hostname == 'localhost'){
         return true;
@@ -9,7 +9,30 @@ app.controller('HomeController', ['$scope', 'Restangular',
     }
 
     $scope.nowPlay = function(){
-      
+
     }
+
+    $scope.allVideos = Restangular.all('videos').getList().$object;
+    window.a = $scope.allVideos
+    PubNub.ngSubscribe({
+      channel: 'nowplaying',
+      callback: function(msg, payload){
+        Restangular.all('videos').getList().then(function(videos){
+          $scope.allVideos = videos;
+        }, function(err){
+          console.log('not found')
+        })
+      }
+    })
+
+    $rootScope.newVideo = { url: '' };
+
+    PubNub.ngSubscribe({
+      channel: 'playlist',
+      callback: function(video_id, payload){
+        $scope.allVideos = Restangular.all('videos').getList().$object;
+      }
+    })
+
   }
 ])
--- app/assets/javascripts/app/controllers/MainController.js
+++ app/assets/javascripts/app/controllers/MainController.js
@@ -1,12 +1,10 @@
-app.controller('MainController', ['$scope', '$rootScope', 'Restangular', 'PubNub',
-  function($scope, $rootScope, Restangular, PubNub){
+app.controller('MainController', ['$scope', 'Restangular', 'PubNub',
+  function($scope, Restangular, PubNub){
     PubNub.init({
       publish_key:'pub-c-723e6af6-05bd-4d9c-957b-9ecc8fb3f32d',
       subscribe_key:'sub-c-4e07d514-9c8d-11e5-9a49-02ee2ddab7fe',
       uuid: '1'
     });
-
-    $scope.allVideos = Restangular.all('videos').getList().$object;
 
     if (window.location.hostname == 'localhost') {
       $scope.playVideo = Restangular.one('videos').one('play').get().$object;
@@ -24,26 +22,6 @@ app.controller('MainController', ['$scope', '$rootScope', 'Restangular', 'PubNub
         player.playVideo();
       })
     }
-
-    PubNub.ngSubscribe({
-      channel: 'nowplaying',
-      callback: function(msg, payload){
-        Restangular.all('videos').getList().then(function(videos){
-          $scope.allVideos = videos;
-        }, function(err){
-          console.log('not found')
-        })
-      }
-    })
-
-    $rootScope.newVideo = { url: '' };
-
-    PubNub.ngSubscribe({
-      channel: 'playlist',
-      callback: function(video_id, payload){
-        $scope.allVideos = Restangular.all('videos').getList().$object;
-      }
-    })
 
   }
 ])
