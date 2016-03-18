2300081456b9feb44aa6475071636d2fa4c7b821
Dimas Julisa Taniawan <dimas@41studio.com>
2016-03-18 17:04:36 +0700

    fix loggin

--- app/assets/javascripts/app/controllers/DjsController.js
+++ app/assets/javascripts/app/controllers/DjsController.js
@@ -4,8 +4,7 @@ app.controller('DjsController', ['$scope', '$rootScope', '$location', 'Restangul
     channelId = $stateParams.id
     $rootScope.dj_logged = $cookies.get('dj')
 
-    // logged dj
-    if ($rootScope.dj_logged != undefined) {
+    initChannel = function(){
       $scope.MainYoutube = MainYoutube
 
       Restangular.one('channels', channelId).get().then(function(channel){
@@ -56,24 +55,30 @@ app.controller('DjsController', ['$scope', '$rootScope', '$location', 'Restangul
       }
     }
 
-    // Check channel status if active change isChannelActive
-    //if false change to false and if no channel found redirect to root
-    Restangular.all('channels').one(channelId).get().then(function(channel){
-      $scope.isChannelActive = angular.equals(channel.status, 'active')
-      $cookies.put('dj', channelId)
-    }, function(){
-      $location.path('/')
-    })
-
-    $scope.validatePassword = function(){
-      password = document.getElementById('password').value
-      Restangular.one('channels', channelId).post('sign_in', {password: password}).then(function(channel){
-        $rootScope.login_pass = channel.is_password_valid
-
-        ngToast.success("Log in success")
-      }, function(err){
-        ngToast.danger("password is incorrect")
+    // logged dj
+    if ($rootScope.dj_logged != undefined) {
+      initChannel()
+    }else{
+      // Check channel status if active change isChannelActive
+      //if false change to false and if no channel found redirect to root
+      Restangular.all('channels').one(channelId).get().then(function(channel){
+        return true
+      }, function(){
+        $location.path('/')
       })
+
+      $scope.validatePassword = function(){
+        password = document.getElementById('password').value
+        Restangular.one('channels', channelId).post('sign_in', {password: password}).then(function(channel){
+          $rootScope.dj_logged = channelId
+          $cookies.put('dj', channelId)
+          initChannel()
+
+          ngToast.success("Log in success")
+        }, function(err){
+          ngToast.danger("password is incorrect")
+        })
+      }
     }
 
     controlVideos = function(type){
