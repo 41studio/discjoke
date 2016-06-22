8bc7d7f894155284dbf28ce88170ad49774b0c0e
Aditia Mahdar <adit@41studio.com>
2016-06-22 22:34:26 +0700

    Update YT config

--- config/initializers/yt.rb
+++ config/initializers/yt.rb
@@ -1,9 +1,6 @@
 Yt.configure do |config|
   config.log_level = :debug
-# config.api_key = 'AIzaSyDA0mUK0XcurAwi5ijOpzcDP1X2Bek67s0'
-# config.client_id = '2039945053-se1g2167pa6snqa7mkl66ru7k77otnaq.apps.googleusercontent.com'
-# config.client_secret = 'tfT1bSk12UblKNansFdosFHc'
-  config.api_key = 'AIzaSyD5wAYYpuY0geyU67AxSFmNI6JwZrRxs4k'
-  config.client_id = '1068274806366-iekeif8fgbovf558uo7qo3q8m3ift4pn.apps.googleusercontent.com'
-  config.client_secret = 'MqAWBje2a4gYl25Uo5yRJtlP'
+  config.api_key = 'AIzaSyBLSPCOxF-Am7c-Ubz4SiFJORASk0ifSgU'
+  config.client_id = '430925043258-q42153gcl43tlknq029ap48sbl8k4bb3.apps.googleusercontent.com'
+  config.client_secret = 'uPjVrwjO_AGAu1iU8HEswzRq'
 end
